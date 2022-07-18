2a96e40b2dc7562e51d88e6698a572b4ee6015be
Fede Cione <fede_cione_96@hotmail.com>
2022-07-18 14:00:36 -0300

    Random gifs app

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import '../App.css';
 import '../assets/css/app.css';
 import PageContent from './PageContent';
+const apiKey = 'Here put your Giphy api key';
 
 class App extends Component {
   constructor(props) {
@@ -13,12 +14,13 @@ class App extends Component {
 
   async ApiCall(url) {
     let gifsArray = [];
-    for(let i = 0; i < 4; i++) {
+    for(let i = 0; i < 8; i++) {
       let randomNumber = Math.floor(Math.random() * 50);
        await fetch(url)
         .then(response => response.json())
         .then(data => {
-          gifsArray.push(data.data[randomNumber].images.downsized.url);
+          console.log(data)
+          gifsArray.push(data.data[randomNumber].images.original.url);
         })
         .catch(error => console.log(error))
     }
@@ -26,7 +28,7 @@ class App extends Component {
   }
 
   getNewGif() {
-    this.ApiCall('https://api.giphy.com/v1/gifs/trending?api_key={Your Giphy Key}');
+    this.ApiCall(`https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}`);
   }
  
   showGifs(data) {
@@ -46,15 +48,15 @@ class App extends Component {
         <React.Fragment>
           <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
             <div className="container">
-                <a className="navbar-brand" href="localhost:3000">GIPHY APP</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <a className="navbar-brand" href="localhost:3000">RANDOM GIFS APP</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
             </div>
-            <div className="collapse navbar-collapse" id="navbarResponsive">
+            <div className="collapse navbar-collapse">
                 <ul className="nav ml-auto">
                     <li className="nav-item">
-                        <button className="btn btn-success" onClick={() => this.getNewGif()}>Cargar random</button>
+                        <button className="btn btn-success" onClick={() => this.getNewGif()}>Random Gifs</button>
                     </li>
                 </ul>
             </div>
